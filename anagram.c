d0318e11a624d196617dbbff22eddadcd8516762
KimonSMN <kimonsmirlianos@gmail.com>
2024-03-03 12:29:43 +0200

    finished

--- anagram.c
+++ anagram.c
@@ -1,37 +1,53 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool isAnagram(char *s, char *t)
 {
-    int sizeOfS = sizeof(s) / sizeof(s[0]);
-    int sizeOfT = sizeof(t) / sizeof(t[0]);
-    int i, j;
-    if (sizeOfS != sizeOfT)
-        return false;
+    int sLength = strlen(s);
+    int tLength = strlen(t);
 
-    for (i = 0; i < sizeOfS; i++)
+    if (sLength != tLength)
     {
-        for (j = 0; j < sizeOfS; j++)
-        {
-            if (s[j] > s[j + 1] && (t[j] > t[j + 1]))
-            {
-                int temp = s[j];
-                s[j] = s[j + 1];
-                s[j + 1] = temp;
-            }
-        }
+        return false;
     }
 
-    for (i = 0; i < sizeOfS; i++)
+    int charCount[256] = {0};
+
+    // Count characters in s
+    for (int i = 0; i < sLength; i++)
     {
-        if (s[i] != t[i])
+        charCount[s[i]]++;
+    }
+
+    // Subtract character count based on t
+    for (int i = 0; i < tLength; i++)
+    {
+        charCount[t[i]]--;
+
+        // If count becomes negative, t has an extra character not in s
+        if (charCount[t[i]] < 0)
+        {
             return false;
+        }
     }
     return true;
 }
 
 int main()
 {
+    char *str1 = "lisen";
+    char *str2 = "silent";
+
+    bool result = isAnagram(str1, str2);
+    if (result)
+    {
+        printf("The strings are anagrams.\n");
+    }
+    else
+    {
+        printf("The strings are not anagrams.\n");
+    }
 
     return 0;
 }
