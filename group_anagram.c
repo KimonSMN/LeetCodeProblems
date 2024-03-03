a0dc08cee62a92c8d2b1eba98a50a07ffaecb97e
KimonSMN <kimonsmirlianos@gmail.com>
2024-03-03 13:41:25 +0200

    progress

--- group_anagram.c
+++ group_anagram.c
@@ -12,15 +12,39 @@ char ***groupAnagrams(char **strs, int strsSize, int *returnSize, int **returnCo
 {
     for (int i = 0; i < strsSize; i++) // Iterate through all elements of strs
     {
-        int charCount[256] = {0};            // Reset for each string
-        int tempLength = strlen(strs[i]);    // Find length of each strs element
-        for (int j = 0; j < tempLength; j++) // Iterate through all characters in each strs element
+        int charCount1[256] = {0};            // Reset for each string
+        int tempLength1 = strlen(strs[i]);    // Find length of each strs element
+        for (int j = 0; j < tempLength1; j++) // Iterate through all characters in each strs element
         {
-            charCount[(unsigned char)strs[i][j]]++;
+            charCount1[(unsigned char)strs[i][j]]++;
         }
+
+        for (int k = 0; k < strsSize; k++)
+        {
+            if (i == k)
+                continue;              // Skip comparing the string with itself
+            int charCount2[256] = {0}; // Character counts for strs[k]
+
+            int tempLength2 = strlen(strs[k]);
+            for (int l = 0; l < tempLength2; l++)
+            {
+                charCount2[(unsigned char)strs[k][l]]++;
+            }
+
+            bool isAnagram = true;
+            for (int m = 0; m < 256; m++)
+            {
+                if (charCount1[m] != charCount2[m])
+                {
+                    isAnagram = false;
+                    break;
+                }
+            }
+        }
+        
     }
 }
-
 int main()
 {
+    return 0;
 }
