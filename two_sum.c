b1e0060d8f9ffa17c30c7b14b6f072298207daca
KimonSMN <kimonsmirlianos@gmail.com>
2024-03-03 12:43:39 +0200

    update
    a number larger than the target can still be a part of the solution when combined with a negative number.
    so i removed the tempArray

--- two_sum.c
+++ two_sum.c
@@ -4,18 +4,10 @@
 
 int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 {
-    int tempArr[numsSize];
-
-    // Create new array with only possible numbers for combination
-    for (int i = 0; i <= numsSize; i++)
-    {
-        if (nums[i] < target)
-            tempArr[i] += nums[i];
-    }
 
     for (int i = 0; i <= numsSize; i++)
     {
-        for (int j = 0; j <= numsSize - 1; j++)
+        for (int j = i + 1; j <= numsSize; j++)
         {
             if (nums[i] + nums[j] == target)
             {
@@ -28,4 +20,21 @@ int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 
 int main()
 {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int returnSize[2] = {-1, -1}; // Initialize with -1 to indicate not found
+
+    twoSum(nums, numsSize, target, returnSize);
+
+    if (returnSize[0] != -1 && returnSize[1] != -1)
+    {
+        printf("Indices: %d, %d\n", returnSize[0], returnSize[1]);
+    }
+    else
+    {
+        printf("No two sum solution found.\n");
+    }
+
+    return 0;
 }
