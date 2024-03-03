c0df21fd685d62c4b5daa1db6a7b2a61800bcf53
KimonSMN <kimonsmirlianos@gmail.com>
2024-03-03 13:11:17 +0200

    progress

---
+++ group_anagram.c
@@ -0,0 +1,28 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+char ***groupAnagrams(char **strs, int strsSize, int *returnSize, int **returnColumnSizes)
+{
+
+    int charCount[256] = {0};
+
+    for (int i = 0; i < strsSize; i++) // Iterate through all elements of strs
+    {
+        int tempLength = strlen(strs[i]);    // Find length of each strs element
+        for (int j = 0; j < tempLength; j++) // Iterate through all characters in each strs element
+        {
+            charCount[strs[]]++;
+        }
+    }
+}
+
+int main()
+{
+}
